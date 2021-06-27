428ccfd04877c0c0ef30ced1182d18f308345650
RakshanCB <rakshu2c@gmail.com>
2021-06-27 18:45:27 +0530

    Vanishing Pigs

--- Bird.js
+++ Bird.js
@@ -2,6 +2,9 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+
+    Matter.Body.setMass(this.body,this.body.mass*3);
+    
   }
 
   display() {
